857085b962f8ac64a69faabc6011cb1778961976
Dharmeshp1582 <alpeshpatelvirpur@gmail.com>
2025-07-16 13:55:55 +0530

    admin side completed

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+import "dotenv/config"
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
+const app = express();
+const port = 3000;
+
+await connectDB()
+
+
+//middleware
+app.use(express.json())
+app.use(cors());
+app.use(clerkMiddleware())
+
+
+//Api route
+app.get('/',(req,res)=> {
+  res.send('Server is started ')
+})
+
+app.use('/api/inngest', serve({ client: inngest, functions }))
+
+app.listen(port,()=> {
+  console.log(`Server listing at ${port}`)
+})
